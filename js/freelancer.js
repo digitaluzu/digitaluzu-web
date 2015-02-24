87038f8badcf8add13aca0157f0781e79e074731
Shyam Guthikonda <shyamguth@gmail.com>
2015-02-24 12:09:33 +0900

    Import.

---
+++ js/freelancer.js
@@ -0,0 +1,49 @@
+/*!
+ * Start Bootstrap - Freelancer Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+var scrollOffset = 40;
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - (scrollOffset - 1)
+        }, 1000, 'easeInOutQuart');
+        event.preventDefault();
+    });
+});
+
+// Floating label headings for the contact form
+$(function() {
+    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+        $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
+    }).on("focus", ".floating-label-form-group", function() {
+        $(this).addClass("floating-label-form-group-with-focus");
+    }).on("blur", ".floating-label-form-group", function() {
+        $(this).removeClass("floating-label-form-group-with-focus");
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    offset: $(window).height() * 0.5,
+    target: '.navbar-fixed-top'
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+$(document).ready(function(){
+    // Email address obfuscation:
+    var e = 'contact';
+    var t = 'digitaluzu';
+    var n = '.com';
+    var r = 'mailto:' + e + '@' + t + n;
+    $(".contact-email").attr("href", r);
+});
